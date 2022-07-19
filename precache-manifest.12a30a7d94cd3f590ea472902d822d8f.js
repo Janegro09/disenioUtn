07b72b8170e122c52c26703c8c72e5a9f1e1aff2
janegro09 <javier.sosa099@gmail.com>
2022-07-19 15:29:32 -0300

    Updates

---
+++ precache-manifest.12a30a7d94cd3f590ea472902d822d8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6731226148afd901c7d6",
+    "url": "/disenioUtn/static/css/main.7e76d1b5.chunk.css"
+  },
+  {
+    "revision": "6731226148afd901c7d6",
+    "url": "/disenioUtn/static/js/main.67312261.chunk.js"
+  },
+  {
+    "revision": "fb3c7a63a7bf9afdde87",
+    "url": "/disenioUtn/static/js/1.fb3c7a63.chunk.js"
+  },
+  {
+    "revision": "379a362705ec5d7700d9",
+    "url": "/disenioUtn/static/js/runtime~main.379a3627.js"
+  },
+  {
+    "revision": "4d891fe05f89f779e833ad5cab330953",
+    "url": "/disenioUtn/index.html"
+  }
+];
